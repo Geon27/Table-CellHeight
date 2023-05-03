eb5e6489e784e0fadcc4a94ed8da8d885f6fc497
Geon_27 <yukgeon98527@gmail.com>
2023-05-04 00:24:26 +0900

    23/05/04 code added

--- Table-CellHeight/ListViewController.swift
+++ Table-CellHeight/ListViewController.swift
@@ -10,5 +10,7 @@ class ListViewController: UITableViewController {
     // 테이브 뷰에 연결된 데이터를 저장하는 배열
     var list = [String]()
     @IBAction func add(_ sender: Any) {
+    // 알림창 객체의 입력폼을 추가한다
+        let alert = UIAlertController(title: "목록입력", message: "추가될 글을 작성해주세요", preferredStyle: .alert)
     }
 }
